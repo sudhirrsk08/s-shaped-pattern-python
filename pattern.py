d373c0b2b3b9e28641f03655351d84213ce5e967
sudhirrsk08 <sudhirkumar.r08@gmail.com>
2019-08-20 10:44:31 +0530

    pattern

---
+++ pattern.py
@@ -0,0 +1,16 @@
+def flat():
+	for i in range(0,2):
+		for j in range(0,10): 
+			print('* ',end="")
+		print("\n ")
+def flat2():
+	for i in range(0,1):
+        	for j in range(0,5):
+                	print('*   *')
+flat()
+flat2()
+flat()
+for i in range(0,1):
+	for k in range(0,5):
+		print("              *   *")
+flat()
